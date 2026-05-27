3814faf95de1f1009e3ca5d387437d8a1b5dde7e
Luciano Emmanuel Gatti Flekenstein <138601174+Luciano-Gatti@users.noreply.github.com>
2026-05-27 11:08:04 -0300

    Implementa vistas de historial de deuda y observaciones de expediente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,8 @@ import Dashboard from "./pages/Dashboard";
 import Inmuebles from "./pages/Inmuebles";
 import InmuebleDetalle from "./pages/InmuebleDetalle";
 import HistorialSeguimiento from "./pages/HistorialSeguimiento";
+import HistorialDeudaInmueble from "./pages/HistorialDeudaInmueble";
+import ObservacionesExpedienteInmueble from "./pages/ObservacionesExpedienteInmueble";
 import GestionDeuda from "./pages/GestionDeuda";
 import CargaDetalle from "./pages/CargaDetalle";
 import GestionEtapas from "./pages/GestionEtapas";
@@ -33,6 +35,8 @@ const App = () => (
             <Route path="/inmuebles" element={<Inmuebles />} />
             <Route path="/inmuebles/:id" element={<InmuebleDetalle />} />
             <Route path="/inmuebles/:id/seguimiento" element={<HistorialSeguimiento />} />
+            <Route path="/inmuebles/:id/historial-deuda" element={<HistorialDeudaInmueble />} />
+            <Route path="/inmuebles/:id/observaciones-expediente" element={<ObservacionesExpedienteInmueble />} />
             <Route path="/deuda" element={<GestionDeuda />} />
             <Route path="/deuda/:id" element={<CargaDetalle />} />
             <Route path="/etapas" element={<GestionEtapas />} />
